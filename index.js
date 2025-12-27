bf4022913a15838c38d28ddb1b3b907a45483cd6
shreyaasingh02 <shreyadsingh1202@gmail,com>
2025-12-27 10:22:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const pass = document.querySelector(".password");
+const toggle = document.querySelector(".toggle");
+
+toggle.addEventListener("click", () => {
+  if (pass.type === "password") {
+    pass.type = "text";
+    toggle.classList.remove("fa-eye-slash");
+    toggle.classList.add("fa-eye");
+  } else {
+    pass.type = "password";
+    toggle.classList.remove("fa-eye");
+    toggle.classList.add("fa-eye-slash");
+  }
+});
+
+
+let forpass = document.querySelector(".fpass");
+forpass.onclick = () => {
+    console.log("Clicked");
+    window.location.href = "forget-pass.html";
+}
+
+const card = document.querySelector(".main-card");
